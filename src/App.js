830f0bb64062049a0df2d51b3fa71eb48e7846d7
yangxin <3287723532@qq.com>
2020-03-20 14:45:33 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import './App.css';
+import * as cartActions from './actions/cart'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    let { shops } = this.props.cart;
+    return (
+      <div className="App">
+        <ul>
+          {shops.map((s, index) => {
+            return <li key={index}>{s.product}===>{s.quantity}===>{s.unitCost}</li>
+          })}
+        </ul>
+        <hr />
+        <button onClick={() => this.props.addCart()} > 增加商品</button>
+        <button onClick={() => this.props.updateCart()} > 修改商品</button>
+        <button onClick={() => this.props.deleteCart()} > 删除商品</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+function getState(state) {
+  return {
+    cart: state.cart
+  }
+}
+
+function getDispatch(dispatch) {
+  return {
+    addCart: () => dispatch(cartActions.addCart("鱼香肉丝", 1, 20)),
+    updateCart: () => dispatch(cartActions.updateCart("鱼香肉丝", 2, 50)),
+    deleteCart: () => dispatch(cartActions.deleteFromCart("鱼香肉丝"))
+  }
+}
+
+export default connect(getState, getDispatch)(App);
